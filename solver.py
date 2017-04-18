01f883e219e176e8cfd38f6b0a8d2540b62a1f55
yyy <y.w.k.w.1717@gmail.com>
2017-04-19 02:38:08 +0900

    [Add] Cube class

--- solver.py
+++ solver.py
@@ -6,15 +6,34 @@ import ConfigParser
 inifile = ConfigParser.SafeConfigParser()
 inifile.read('./config.ini')
 
-U = inifile.get('settings', 'U')
-D = inifile.get('settings', 'D')
-R = inifile.get('settings', 'R')
-L = inifile.get('settings', 'L')
-F = inifile.get('settings', 'F')
-B = inifile.get('settings', 'B')
+
+class Cube:
+    """ The Cube Class"""
+    """ solved cube layout is UUUULLLLFFFFRRRRBBBBDDDD """
+
+    # コンストラクタ
+    def __init__(self, layout):
+        self.layout = list(layout)  # 文字列をリストに変換
+        self.colors = [
+            inifile.get('settings', 'U'),
+            inifile.get('settings', 'L'),
+            inifile.get('settings', 'F'),
+            inifile.get('settings', 'R'),
+            inifile.get('settings', 'B'),
+            inifile.get('settings', 'D')
+        ]
+
+    def getLayout(self):
+        return self.layout
+
+    def getColor(self, num):
+        return self.colors[num]
+
 
 def main():
-    print U, D, R, L, F, B
+    cube = Cube('BUFFDDLLRRDFURDFBRLUBLBU')
+    print 'U: ' + cube.getColor(0)
+    print cube.getLayout()
 
 
 if __name__ == '__main__':
